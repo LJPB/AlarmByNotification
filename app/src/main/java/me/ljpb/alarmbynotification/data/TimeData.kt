1b6727477e1737096dcdd0fb895e63a6d6ae2b28
ljpb <dev@ljpb.me>
2024-07-10 22:05:40 +0900

    通知が届く予定の時間をエポック秒ではなくLocalDateTimeで管理

--- app/src/main/java/me/ljpb/alarmbynotification/data/TimeData.kt
+++ app/src/main/java/me/ljpb/alarmbynotification/data/TimeData.kt
@@ -1,16 +1,18 @@
 package me.ljpb.alarmbynotification.data
 
+import java.time.LocalDateTime
+
 /** セットしたタイマーやアラームを表示するためのデータを表すクラス
  * @param id 一意のID
  * @param name タイマーやアラームの名称
  * @param type タイマー/アラームの種類
- * @param epochSeconds 通知が届く予定のエポック秒
+ * @param finishDateTime 通知が届く予定の日時
  */
 data class TimeData(
     val id: Int,
     val name: String,
     val type: TimeType,
-    var epochSeconds: Long,
+    var finishDateTime: LocalDateTime,
 )
 
 enum class TimeType {
