82536b948e72aa9ae67268259cba16294dfa08b7
ljpb <dev@ljpb.me>
2024-07-10 21:47:38 +0900

    セットしたタイマーやアラームを表示するためのデータを表すクラスの追加

--- app/src/main/java/me/ljpb/alarmbynotification/data/TimeData.kt
+++ app/src/main/java/me/ljpb/alarmbynotification/data/TimeData.kt
@@ -1,5 +1,18 @@
 package me.ljpb.alarmbynotification.data
 
+/** セットしたタイマーやアラームを表示するためのデータを表すクラス
+ * @param id 一意のID
+ * @param name タイマーやアラームの名称
+ * @param type タイマー/アラームの種類
+ * @param epochSeconds 通知が届く予定のエポック秒
+ */
+data class TimeData(
+    val id: Int,
+    val name: String,
+    val type: TimeType,
+    var epochSeconds: Long,
+)
+
 enum class TimeType {
     Alarm,
     Timer
