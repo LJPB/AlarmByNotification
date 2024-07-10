c6d6186c4907fc6d7b074935c4a00e2502615500
ljpb <dev@ljpb.me>
2024-07-11 05:00:43 +0900

    notificationテーブルのDaoを追加

---
+++ app/src/main/java/me/ljpb/alarmbynotification/data/NotificationDao.kt
@@ -0,0 +1,27 @@
+package me.ljpb.alarmbynotification.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NotificationDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(notification: NotificationEntity)
+
+    @Update
+    suspend fun update(notification: NotificationEntity)
+ 
+    @Delete
+    suspend fun delete(notification: NotificationEntity)
+    
+    @Query("select * from notifications where notifyId = :id")
+    fun getItem(id: Int): Flow<NotificationEntity>
+    
+    @Query("select * from notifications order by triggerTime asc")
+    fun getAllItem(id: Int): List<Flow<NotificationEntity>>
+}
