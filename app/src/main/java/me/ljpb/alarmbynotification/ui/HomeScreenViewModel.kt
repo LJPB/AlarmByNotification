a4b92f0ba0a9fb9099746c75dd94261bc7a36faa
ljpb <dev@ljpb.me>
2024-07-16 19:25:14 +0900

    セットした時刻の削除機能の追加

--- app/src/main/java/me/ljpb/alarmbynotification/ui/HomeScreenViewModel.kt
+++ app/src/main/java/me/ljpb/alarmbynotification/ui/HomeScreenViewModel.kt
@@ -7,9 +7,11 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import me.ljpb.alarmbynotification.data.NotificationRepositoryInterface
 import me.ljpb.alarmbynotification.data.TimeData
 import java.time.Instant
@@ -22,13 +24,13 @@ const val DELAY_TIME: Long = 100L
 class HomeScreenViewModel(private val repository: NotificationRepositoryInterface) : ViewModel() {
     private val _currentDateTime = MutableStateFlow(LocalDateTime.now())
     val currentDateTime: StateFlow<LocalDateTime> = _currentDateTime.asStateFlow()
-    
+
     val setTimeList: StateFlow<List<TimeData>> = repository
         .getAllNotifications()
         .map { notificationEntities ->
             if (notificationEntities.isNullOrEmpty()) return@map listOf()
             notificationEntities.map { notification ->
-                val zonedDateTime = Instant.ofEpochSecond(notification.triggerTime)
+                val zonedDateTime = Instant.ofEpochSecond(notification.triggerTimeMilliSeconds)
                     .atZone(ZoneId.systemDefault())
                     .toLocalDateTime()
                 TimeData(
@@ -44,12 +46,30 @@ class HomeScreenViewModel(private val repository: NotificationRepositoryInterfac
             started = SharingStarted.WhileSubscribed(2_000L),
             initialValue = listOf()
         )
-    
+
     suspend fun updateCurrentDateTime() {
         while (true) {
             _currentDateTime.update { LocalDateTime.now() }
             delay(DELAY_TIME)
         }
     }
-    
+
+    fun setTitle() {
+
+    }
+
+    fun changeTime() {
+
+    }
+
+     fun delete(timeData: TimeData) {
+         viewModelScope.launch {
+             val notify = repository
+                 .getNotification(timeData.id)
+                 .firstOrNull()
+             if (notify != null) {
+                 repository.deleteNotification(notify)
+             }
+         }
+    }
 }
