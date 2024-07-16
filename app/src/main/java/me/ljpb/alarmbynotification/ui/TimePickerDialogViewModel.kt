a0721583ab617d4398cde5e673027316672004cf
ljpb <dev@ljpb.me>
2024-07-16 18:55:25 +0900

    通知のデータベースへの追加機能の実装

--- app/src/main/java/me/ljpb/alarmbynotification/ui/TimePickerDialogViewModel.kt
+++ app/src/main/java/me/ljpb/alarmbynotification/ui/TimePickerDialogViewModel.kt
@@ -7,13 +7,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import me.ljpb.alarmbynotification.data.NotificationEntity
 import me.ljpb.alarmbynotification.data.NotificationRepositoryInterface
+import me.ljpb.alarmbynotification.data.TimeType
 import java.time.LocalTime
+import java.time.ZonedDateTime
+import java.util.UUID
 
 class TimePickerDialogViewModel(
     private val repository: NotificationRepositoryInterface,
     app: Application,
-    ) : AndroidViewModel(app) {
+) : AndroidViewModel(app) {
     // アラームにセットする時刻を管理する
     @OptIn(ExperimentalMaterial3Api::class)
     lateinit var alarmState: TimePickerState
@@ -34,12 +40,6 @@ class TimePickerDialogViewModel(
         setAlarmInitialState()
         setTimerInitialState()
     }
-/*
-    fun showDialog(hour: Int? = null, min: Int? = null, is24Hour: Boolean? = null) {
-        isShow = true
-        setAlarmInitialState(hour = hour, min = min, is24Hour = is24Hour)
-    }
- */
 
     fun showAlarmDialog(hour: Int? = null, min: Int? = null, is24Hour: Boolean? = null) {
         isShow = true
@@ -55,6 +55,59 @@ class TimePickerDialogViewModel(
 
     fun hiddenDialog() {
         isShow = false
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun add() {
+        val type: TimeType
+        val triggerTimeSeconds: Long
+        val currentDateTime = ZonedDateTime.now()
+
+        if (isAlarm) {
+            type = TimeType.Alarm
+
+            val setTimeAsMinute = alarmState.hour * 60 + alarmState.minute
+            val currentTimeAsMinute = currentDateTime.hour * 60 + currentDateTime.minute
+
+            // セットした時刻と(日付を無視した)現在時刻の差
+            val dif = setTimeAsMinute - currentTimeAsMinute
+           
+            val addMinutes = if (dif > 0) {
+                // セットした時刻 - (日付を無視した)現在時刻 > 0なら，セットした時刻は現在と同じ日付の時刻ということ
+                // よって差分を足せば，現在の日付でセットした時刻を得られる
+                dif.toLong()
+            } else if (dif < 0) {
+                // セットした時刻 - (日付を無視した)現在時刻 < 0なら，セットした時刻は現在の翌日の時刻ということ
+                // よって，現在の1日後から差分の絶対値を引けば(difは負だから+difする)，現在の翌日でセットした時刻を得られる
+                (24 * 60 + dif).toLong()
+            } else {
+                0  // todo
+            }
+            triggerTimeSeconds = currentDateTime
+                .plusMinutes(addMinutes)
+                .minusSeconds(currentDateTime.second.toLong())  // 秒は無視する
+                .minusNanos(currentDateTime.nano.toLong())
+                .toEpochSecond()
+        
+        } else {
+            type = TimeType.Timer
+            triggerTimeSeconds = currentDateTime
+                .plusHours(timerState.hour.toLong())
+                .plusMinutes(timerState.minute.toLong())
+                .toEpochSecond()
+        }
+
+        val notifyId = UUID.randomUUID().hashCode()
+        val notification = NotificationEntity(
+            notifyId = notifyId,
+            title = "",
+            text = "",
+            triggerTimeMilliSeconds = triggerTimeSeconds * 1000,
+            type = type,
+        )
+        viewModelScope.launch {
+            repository.insertNotification(notification)
+        }
     }
 
     /**
@@ -110,13 +163,16 @@ class TimePickerDialogViewModel(
      * @param is24Hour 24時間表示かどうか, null(未指定)なら端末の設定による
      */
     @OptIn(ExperimentalMaterial3Api::class)
-    private fun setAlarmStateFromTimerStateBasedCurrentTime(timePickerState: TimePickerState = this.timerState, is24Hour: Boolean? = null) =
+    private fun setAlarmStateFromTimerStateBasedCurrentTime(
+        timePickerState: TimePickerState = this.timerState,
+        is24Hour: Boolean? = null
+    ) =
         setAlarmStateFromTimerStateBasedCurrentTime(
             timerHour = timePickerState.hour,
             timerMin = timePickerState.minute,
             is24Hour = is24Hour
         )
-    
+
     /** 現在時刻を基準にタイマーにセットした時間をアラームの時刻に変換してalarmStateにセットする
      * たとえば現在10:00で，タイマーに90分(1時間30分)とセットしていた場合，11:30に変換してセットする
      * @param timerHour タイマーにセットした時間
@@ -146,12 +202,12 @@ class TimePickerDialogViewModel(
             is24Hour = is24Hour
         )
     }
-    
+
     @OptIn(ExperimentalMaterial3Api::class)
     private fun setAlarmInitialState(
         hour: Int? = null,
         min: Int? = null,
-        is24Hour: Boolean? = null
+        is24Hour: Boolean? = null,
     ) {
         val currentTime = LocalTime.now()
         val initialHour: Int
