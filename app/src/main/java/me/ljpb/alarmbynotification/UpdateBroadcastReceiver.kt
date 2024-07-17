020029f7af6c20c21f273396d4af386842b5217a
ljpb <dev@ljpb.me>
2024-07-17 21:38:05 +0900

    アプリ更新時に通知を再設定するようにした

---
+++ app/src/main/java/me/ljpb/alarmbynotification/UpdateBroadcastReceiver.kt
@@ -0,0 +1,42 @@
+package me.ljpb.alarmbynotification
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
+
+class UpdateBroadcastReceiver : BroadcastReceiver() {
+    @OptIn(DelicateCoroutinesApi::class)
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (context == null || intent == null) return
+
+        if (intent.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
+            
+            val application = context.applicationContext as NotificationApplication
+            val repository = application.container.notificationRepository
+            
+            val pendingResult = goAsync()
+            GlobalScope.launch {
+                try {
+                    repository
+                        .getAllNotifications()
+                        .firstOrNull()
+                        ?.forEach {
+                            val currentTime = System.currentTimeMillis()
+                            // 過ぎていたら
+                            if (it.triggerTimeMilliSeconds > currentTime) {
+                                deleteNotification(context, notificationInfo = it)
+                            } else {
+                                setNotification(context = context, notificationInfo = it)
+                            }
+                        }
+                } finally {
+                    pendingResult.finish()
+                }
+            }
+        }
+    }
+}
