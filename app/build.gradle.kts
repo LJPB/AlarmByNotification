e21a68522867286df8c4228c2cfa59ffb40664c4
ljpb <dev@ljpb.me>
2024-07-28 01:58:55 +0900

    修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.ljpb.alarmbynotification"
         minSdk = 31
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
